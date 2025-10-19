60da842a5b75b86c423e7246cce4c91aa596a766
ZxcSkaur <danlisguuuu@gmail.com>
2025-10-19 22:02:51 +0300

    Add files via upload

---
+++ FormManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.model;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormManager : Form
+    {
+        public FormManager()
+        {
+            InitializeComponent();
+        }
+
+        private void FormManager_Load(object sender, EventArgs e)
+        {
+            Model1 model = new Model1();
+            // Используем ранее заполненное статическое поле формы авторизации для заполнения этой формы данными
+            label1.Text = FormAutorization.Enter_User.First_Name + " " + FormAutorization.Enter_User.Second_Name;
+            label2.Text = model.Roles.First(x => x.ID == FormAutorization.Enter_User.RoleID).Name;
+            pictureBox1.Image = Image.FromFile(@"Photo\" + FormAutorization.Enter_User.Pictures);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
